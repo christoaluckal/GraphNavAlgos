bc2ce7a27aa1cae3058d2d80e0432ba3ee47025b
christoaluckal <christo12aluckal@gmail.com>
2020-09-27 23:18:09 +0530

    updated KDtree

--- kdtree.py
+++ kdtree.py
@@ -3,59 +3,61 @@ from scipy.spatial import KDTree
 # from random import randint as rd
 import matplotlib.pyplot as plt
 import time
-x = []
-y = []
-fin = []
+final_coords_list = []
 pointlist = {}
 
+#Sample Room X,Y Coordinates
 x_t = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
 
 y_t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 
+#One vertical line and one horizontal line
 # x_t = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ]
 # y_t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60]
 
-
+#Iterate through the point list and store the coordinates in a dictionary for future reference
 for i in range(0,len(x_t)):
     xval = x_t[i]
     yval = y_t[i]
-    fin.append((xval,yval))
+    final_coords_list.append((xval,yval))
     pointlist['p{}'.format(i)] = [xval,yval]
 
 
-fin = np.array(fin)
-x_coords,y_coords = zip(*fin)
+final_coords_list = np.array(final_coords_list)
+x_coords,y_coords = zip(*final_coords_list)
 plt.scatter(x_coords,y_coords)
 plt.show()
 
 
 start_s = time.time()
-# Creating bounding box where all the points will lie
-kdtree = KDTree(fin)
+#Scipy KDTree implementation
+kdtree = KDTree(final_coords_list)
 
 end_s = time.time()
 
 print(str(end_s-start_s)+" s")
 
-# Dictionary of point name and coordinates
-# print(pointlist)
-current_posx,current_posy = int(input()),int(input()) 
+#Input the position of the observer
+print("Enter the current position X")
+current_posx = int(input())
+print("Enter the current position Y")
+current_posy = int(input()) 
+print("Press WASD to move or press E to exit\n")
 while True:
-    # #Current position
-    # current_posx,current_posy = int(input()),int(input()) 
-    # Return the list of labels that are within the bounding box parameters
     start_s = time.time()
-    idx = kdtree.query_ball_point([current_posx,current_posy],r=10)
+    #Query point is the current location with a radius set to 10 unit distance. Change 10 to whatever is desired
+    coords = kdtree.query_ball_point([current_posx,current_posy],r=10)
     end_s = time.time()
-    # print("Intersection generated in:",str(end_s-start_s)," seconds")
-    # print(key)
-    print(current_posx,current_posy)
-    point_list = fin[idx]
+    #final_coords_list[coords] is a list of coordinate pairs that fall inside the specified distance
+    point_list = final_coords_list[coords]
+    #Incase there are no points in the vicinity we skip the current iteration and ask for next directional input
     if len(point_list) is not 0:
-        temp_x,temp_y = zip(*fin[idx])
-        plt.scatter(temp_x,temp_y)
+        # Split the coords terms to X and Y coordinates
+        plot_x,plot_y = zip(*final_coords_list[coords])
+        plt.scatter(plot_x,plot_y)
         plt.show()
     else:
+        print("Skipped at:",str(current_posx)," ",str(current_posy))
         pass
     #Current position
     next_dir = input()
@@ -67,5 +69,7 @@ while True:
         current_posy = current_posy - 5
     elif next_dir == 'd':
         current_posx = current_posx + 5
+    elif next_dir == 'e':
+        break
     else:
         pass
